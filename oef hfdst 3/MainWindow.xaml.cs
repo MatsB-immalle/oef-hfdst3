c4e29be3cfa869094c1b51063a9a7aba7356b639
mats.bos <mats.bos@immalle.be>
2017-10-03 09:11:19 +0200

    save2

--- oef hfdst 3/MainWindow.xaml.cs
+++ oef hfdst 3/MainWindow.xaml.cs
@@ -24,5 +24,58 @@ namespace oef_hfdst_3
         {
             InitializeComponent();
         }
+
+        private void Mijnknop_Click(object sender, RoutedEventArgs e)
+        {
+            Ellipse ellipse1 = new Ellipse();
+            ellipse1.Width = 50;
+            ellipse1.Height = 10;
+            ellipse1.Margin = new Thickness(10, 10, 0, 0);
+            ellipse1.Stroke = new SolidColorBrush(Colors.Red);
+            ellipse1.Fill = new SolidColorBrush(Colors.Green);
+            Canvas.Children.Add(ellipse1);
+        }
+
+        private void speelveld_Click(object sender, RoutedEventArgs e)
+        {
+            Line lijn = new Line();
+            lijn.X1 = 30;
+            lijn.Y1 = 100;
+            lijn.X2 = 300;
+            lijn.Y2 = 100;
+            lijn.Stroke = new SolidColorBrush(Colors.Black);
+
+            Canvas.Children.Add(lijn);
+
+
+            Line lijn2 = new Line();
+            lijn2.X1 = 30;
+            lijn2.Y1 = 220;
+            lijn2.X2 = 300;
+            lijn2.Y2 = 220;
+            lijn2.Stroke = new SolidColorBrush(Colors.Black);
+
+            Canvas.Children.Add(lijn2);
+
+            Line lijn3 = new Line();
+            lijn3.X1 = 100;
+            lijn3.Y1 = 30;
+            lijn3.X2 = 100;
+            lijn3.Y2 = 300;
+            lijn3.Stroke = new SolidColorBrush(Colors.Black);
+
+            Canvas.Children.Add(lijn3);
+
+            Line lijn4 = new Line();
+            lijn4.X1 = 220;
+            lijn4.Y1 = 30;
+            lijn4.X2 = 220;
+            lijn4.Y2 = 300;
+            lijn4.Stroke = new SolidColorBrush(Colors.Black);
+
+            Canvas.Children.Add(lijn4);
+
+           
+        }
     }
 }
